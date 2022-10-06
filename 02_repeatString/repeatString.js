595e3e75573daa0c9e752e295b53cfe0decc6064
Mehdi Jussm <mehdi.jusseaume@proton.me>
2022-10-06 10:30:48 +0200

    Solve exercises 01 to 04

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,15 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    let newStr = '';
+    
+    if (num < 0) {
+        return 'ERROR'
+    } else {
+        while (num > 0) {
+            newStr += str;
+            num -= 1;
+        }
+        return newStr;
+    }
 
 };
 
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,26 @@
-const removeFromArray = function() {
+const removeFromArray = function(...params) {
 
+    const args = arguments.length - 1;
+    let argsArray = [];
+
+    // Get arguments as an Array
+    for (i = 1; i <= args; i++) {
+        argsArray.splice(1, 0, arguments[i]);
+    } 
+
+    let myArray = arguments[0];
+
+    for (item in argsArray) {
+        let toRemove = myArray.indexOf(argsArray[item]);
+        if (toRemove == -1) { 
+            continue
+        } else {
+            myArray.splice(toRemove, 1);
+        } 
+        
+    }
+
+    return myArray
 };
 
 // Do not edit below this line
